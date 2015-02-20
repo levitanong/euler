8cfc713960888f3dd963e7e3af3257e1da084f4e
Levi Tan Ong <levi@byimplication.com>
2015-02-20 17:05:21 +0800

    complete

--- src/euler/core.clj
+++ src/euler/core.clj
@@ -1,15 +1,21 @@
+; Problem 19
+
 (ns euler.core
   (:gen-class))
 
-(defn divisible-by?
+(defn divisible?
   [x divisor]
   (= (mod x divisor) 0))
 
 (defn leap-year?
   [year]
-  (if (divisible-by? year 100)
-    (divisible-by? year 400) ; special case for century
-    (divisible-by? year 4)))
+  (if (divisible? year 100)
+    (divisible? year 400) ; special case for century
+    (divisible? year 4)))
+
+(defn sunday?
+  [day]
+  (= (mod day 7) 6))
 
 (defn days-in-month
   [year month]
@@ -21,15 +27,21 @@
 (defn gen-months
   [year]
   (for [month (range 12)]
-    (days-in-month year month)))
+    (range 1 (+ (days-in-month year month) 1))))
 
 (defn gen-years
   "return years between 1900 to 2000"
   []
-  (for [year (range 1900 2001)]
-    (gen-months year)))
+  (flatten (for [year (range 1900 2001)]
+    (gen-months year))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println (flatten (gen-years))))
+  (println 
+    (count (filter 
+      (fn [[index item]] (and (sunday? index) (= item 1)))
+      (subvec
+        (vec (map-indexed #(vector %1 %2) (gen-years)))
+        365)
+    ))))
