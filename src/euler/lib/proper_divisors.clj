5365a05992c844dba2839e4611e994aba723fe1f
Levi Tan Ong <levi@byimplication.com>
2015-03-22 21:47:14 +0800

    add definitions of perfect, deficient and abundant numbers

--- src/euler/lib/proper_divisors.clj
+++ src/euler/lib/proper_divisors.clj
@@ -6,3 +6,18 @@
   "The sum of divisors of n less than n. Inefficient but elegant"
   [n]
   (apply + (filter (partial divisible? n) (range 1 n))))
+
+(defn perfect?
+  "The sum of divisors of n less than n is n. "
+  [n]
+  (= n (d n)))
+
+(defn deficient?
+  "d of n is less than n"
+  [n]
+  (< (d n) n))
+
+(defn abundant?
+  "d of n is greater than n"
+  [n]
+  (> (d n) n))
